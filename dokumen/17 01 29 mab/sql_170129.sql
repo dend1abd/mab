d536f5afb3a2c89c7f5e9d2dcd51bcd0e3510abd
dend1abd <dend1abd0907@gmail.com>
2017-01-29 08:54:02 +0700

    revisi 29 jan 17

---
+++ dokumen/17 01 29 mab/sql_170129.sql
@@ -0,0 +1,5 @@
+insert into mst_runno values (6, 'F',100,200,300);
+insert into mst_runno values (7, 'FN',100,200,300);
+insert into mst_runno values (8, 'R',100,200,300);
+insert into mst_runno values (19, 'D',100,200,300);
+select * from mst_runno
